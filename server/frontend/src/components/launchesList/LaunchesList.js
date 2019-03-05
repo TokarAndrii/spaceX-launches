52b7ed644be13b4b6c0adf9a9a66185af3e214fa
tokarandrii <tokar.andrii@gmail.com>
2019-03-05 09:57:17 +0200

    change launch list

--- server/frontend/src/components/launchesList/LaunchesList.js
+++ server/frontend/src/components/launchesList/LaunchesList.js
@@ -40,19 +40,29 @@ export default class LaunchesList extends Component {
 
                         return console.log(data) || data.launches.map(launch => (
                             <div className={styles.launchListItemContent}>
-                            <h3 className={styles.launchListItemContentTitle}>{launch.mission_name}</h3>
-                            <div key={launch.flight_number} className={styles.launcItemHolder}>
-                                {launch.links.mission_patch_small !== null && (<p className={styles.launcItemHolderRowPart}><img className={styles.missionPatch} src={launch.links.mission_patch_small} alt={launch.rocket.rocket_name}></img></p>)}
-                                <p className={styles.launcItemHolderRowPart}><span className={styles.launcItemHolderRowTitleValuetext}>flight_number:</span> <span className={styles.launcItemHolderValueText}>{launch.flight_number}</span> </p>
-                                <p className={styles.launcItemHolderRowPart}><span className={styles.launcItemHolderRowTitleValuetext}>launch_year:</span>  <span className={styles.launcItemHolderValueText}>{launch.launch_year}</span></p>
-                                <p className={styles.launcItemHolderRowPart}><span className={styles.launcItemHolderRowTitleValuetext}>launch_date:</span>  <span className={styles.launcItemHolderValueText}><Moment format="DD-MMMM HH:mm">{launch.launch_date_local}</Moment></span></p>
-                                <p className={styles.launcItemHolderRowPart}>
-                                    <span className={styles.launcItemHolderRowTitleValuetext}>success:</span> <span className={styles.launcItemHolderValueImages}>
-                                        {launch.launch_success ? <img src={successIcon} alt="success_icon"></img> : <img src={failIcon} alt="fail_icon"></img>}</span>
-                                </p>
-                                <p className={styles.launcItemHolderRowPart}><span className={styles.launcItemHolderRowTitleValuetext}>rocket name:</span><span className={styles.launcItemHolderValueText}>{launch.rocket.rocket_name}</span></p>
-                                <p className={styles.launcItemHolderRowPart}><span className={styles.launcItemHolderRowTitleValuetext}>rocket type:</span><span className={styles.launcItemHolderValueText}>{launch.rocket.rocket_type}</span></p>
-                            </div>
+                                <h3 className={styles.launchListItemContentTitle}>{launch.mission_name}</h3>
+                                <div key={launch.flight_number} className={styles.launcItemHolder}>
+                                    {launch.links.mission_patch_small !== null
+                                        && (<p className={styles.launcItemHolderRowPart}><img className={styles.missionPatch} src={launch.links.mission_patch_small} alt={launch.rocket.rocket_name}></img></p>)}
+                                    <p className={styles.launcItemHolderRowPart}>
+                                        <span className={styles.launcItemHolderRowTitleValuetext}>flight_number:</span>
+                                        <span className={styles.launcItemHolderValueText}>{launch.flight_number}</span>
+                                    </p>
+                                    <p className={styles.launcItemHolderRowPart}>
+                                        <span className={styles.launcItemHolderRowTitleValuetext}>launch_year:</span>
+                                        <span className={styles.launcItemHolderValueText}>{launch.launch_year}</span>
+                                    </p>
+                                    <p className={styles.launcItemHolderRowPart}>
+                                        <span className={styles.launcItemHolderRowTitleValuetext}>launch_date:</span>
+                                        <span className={styles.launcItemHolderValueText}><Moment format="DD-MMMM HH:mm">{launch.launch_date_local}</Moment></span>
+                                    </p>
+                                    <p className={styles.launcItemHolderRowPart}>
+                                        <span className={styles.launcItemHolderRowTitleValuetext}>success:</span>
+                                        <span className={styles.launcItemHolderValueImages}>{launch.launch_success ? <img src={successIcon} alt="success_icon"></img> : <img src={failIcon} alt="fail_icon"></img>}</span>
+                                    </p>
+                                    <p className={styles.launcItemHolderRowPart}><span className={styles.launcItemHolderRowTitleValuetext}>rocket name:</span><span className={styles.launcItemHolderValueText}>{launch.rocket.rocket_name}</span></p>
+                                    <p className={styles.launcItemHolderRowPart}><span className={styles.launcItemHolderRowTitleValuetext}>rocket type:</span><span className={styles.launcItemHolderValueText}>{launch.rocket.rocket_type}</span></p>
+                                </div>
                             </div>
                         )
                         );
